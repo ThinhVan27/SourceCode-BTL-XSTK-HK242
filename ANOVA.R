8e95c80a88341f798dfa02557700ae80e302075a
Thinh Van <thinh.vovan@hcmut.edu.vn>
2025-04-21 20:24:50 +0700

    update ANOVA

--- ANOVA.R
+++ ANOVA.R
@@ -17,7 +17,7 @@ boxplot(memory_speed_value ~ manufacturer, data,
         main=expression(bold("Boxplot of Memory_Speed and Manufacturer")),
         col="lightblue")
 table(data$manufacturer)
-# Shapiro-Wilk test cho từng nhóm Manufacturer
+
 # Lấy danh sách các nhà sản xuất duy nhất
 manufacturers <- unique(data$manufacturer)
 
@@ -26,9 +26,6 @@ par(mfrow = c(2, 2))
 
 # Lặp qua từng nhà sản xuất và vẽ Q-Q plot
 for (manu in manufacturers) {
-  cat("Shapiro-Wilk test for", manu, "\n")
-  print(shapiro.test(data$memory_speed_value[data$manufacturer == manu]))
-  
   qqnorm(data$memory_speed_value[data$manufacturer == manu], main = paste("Q-Q Plot for", manu))
   qqline(data$memory_speed_value[data$manufacturer == manu], col = "red")
 }
@@ -41,6 +38,6 @@ leveneTest(memory_speed_value ~ manufacturer, data=data)
 anova_res <- aov(memory_speed_value ~ manufacturer, data = data)
 summary(anova_res)
 
-lsd_test <- LSD.test(anova_res, "manufacturer", p.adj = "none", group = FALSE) 
-print(lsd_test)
-# plot(lsd_test, main = "Fisher's LSD Test for Manufacturer")
+hsd_res <- TukeyHSD(anova_res)
+print(hsd_res)
+plot(hsd_res)
